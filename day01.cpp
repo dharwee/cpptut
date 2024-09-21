5f4a2978eeae8d168e991ccf61f11db038909a8b
dharwee <168101942+dharwee@users.noreply.github.com>
2024-09-21 22:52:59 +0530

    Create day01.cpp

---
+++ day01.cpp
@@ -0,0 +1,4 @@
+#include<iostream>  //header file
+//general; low level lang: close to hardware , high level lang : logically sound; far away from hardware
+/* variables: container to store data
+Data types: defines the type of data a variable will store. It is of 3 types - builtin (int,float,double,boolean),  user defined (Struct,Union,Enum) , derived (Array, pointers,Function)
