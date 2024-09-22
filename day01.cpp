e2cf0cbdeca3a4b6bff25087904524fcf010e2fd
dharwee <168101942+dharwee@users.noreply.github.com>
2024-09-22 20:27:32 +0530

    Update day01.cpp

--- day01.cpp
+++ day01.cpp
@@ -2,3 +2,17 @@
 //general; low level lang: close to hardware , high level lang : logically sound; far away from hardware
 /* variables: container to store data
 Data types: defines the type of data a variable will store. It is of 3 types - builtin (int,float,double,boolean),  user defined (Struct,Union,Enum) , derived (Array, pointers,Function)
+*/
+
+/* Reference variable
+
+int a=843;
+int &b=a;  //b is a reference variable storing the value of a
+cout<<a<<endl<<b;
+
+*/
+
+/* TYPE CASTING
+int a=45.89;
+cout<<"The value of a is: <<int(a);   this will typecast the value of a, and print 45
+  
